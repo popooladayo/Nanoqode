d63b64792e46161e3a1bc638192596c254462b8e
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-12-12 01:16:19 +0100

    Added some colours and the Inter font, changed the website title and logo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "deep-orange": "rgba(218, 69, 44, 1)",
+                "very-light-orange": "rgba(218, 75, 39, 0.12)",
+                "light-blue": "rgba(90, 166, 177, 1)",
+                "very-light-blue": "rgba(235, 244, 246, 1)",
+            },
+            fontFamily: {
+                inter: ["Inter", "serif", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
