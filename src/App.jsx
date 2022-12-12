5a9c52b55288895a59930fcac4ad511ef831c010
iamdayo <ppldayo@gmail.com>
2022-12-12 13:55:13 +0100

    added web-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+
+import Home from './Components/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold ">
-        Hello world!
-      </h1>
-      
+     
+      <Home/>
     </div>
   )
 }
